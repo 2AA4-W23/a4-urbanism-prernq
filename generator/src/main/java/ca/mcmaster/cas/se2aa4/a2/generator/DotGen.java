d1cc7829c7fdcfd46de18ec3790289e1507bac00
prernq <prernasprabhu@gmail.com>
2023-02-19 16:12:52 -0500

    part 1 not working correctly

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -27,13 +27,32 @@ public class DotGen {
                 Double[] coordinates = {(double)x,(double) y};
                 vertex_coords.add(coordinates);
                 System.out.println(coordinates.toString());
-                vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double) y).build());
-                vertices.add(Vertex.newBuilder().setX((double) x).setY((double) y+square_size).build());
-                vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double) y+square_size).build());
+                //vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double) y).build());
+                //vertices.add(Vertex.newBuilder().setX((double) x).setY((double) y+square_size).build());
+                //vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double) y+square_size).build());
             }
         }
-        System.out.println("fThis is here.");
-        System.out.print(vertex_coords.toString().toString());
+        /*
+        Segment[] segments = new Segment[100];
+        for(int x = 0 ; x < vertex_coords.size(); x++){
+            Double[] coordinate_set = vertex_coords.get(x);
+            double x_coord = coordinate_set[0];
+            double y_coord = coordinate_set[1]
+            Segment.newBuilder().
+            segments.add(Segment.newBuilder().setV1Idx(vertices[x]).setV2Idx((int) y_coord).build());
+        }
+        */
+
+        // Create all the edges
+        ArrayList<Segment> segments = new ArrayList<>();
+        for (int x = 0; x < vertices.size() - 1; x++) {
+            segments.add(Segment.newBuilder().setV1Idx(x).setV2Idx(x + 1).build());
+        }
+
+        //System.out.println("fThis is here.");
+        //System.out.print(vertex_coords.toString().toString());
+
+
         // Distribute colors randomly. Vertices are immutable, need to enrich them
         Set<Vertex> verticesWithColors = new HashSet<>();
         Random bag = new Random();
@@ -45,6 +64,23 @@ public class DotGen {
             Property color = Property.newBuilder().setKey("rgb_color").setValue(colorCode).build();
             Vertex colored = Vertex.newBuilder(v).addProperties(color).build();
             verticesWithColors.add(colored);
-        } return Mesh.newBuilder().addAllVertices(verticesWithColors).build();
+        }
+        //return Mesh.newBuilder().addAllVertices(verticesWithColors).build();
+        
+        // Average colour segments
+        ArrayList<Segment> segmentsWithColors = new ArrayList<>();
+        for (Segment s : segments) {
+            int red = bag.nextInt(255);
+            int green = bag.nextInt(255);
+            int blue = bag.nextInt(255);
+            int RedAverage = red / 2;
+            int GreenAverage = green / 2;
+            int BlueAverage = blue / 2;
+            String colorCode = RedAverage + "," + GreenAverage + "," + BlueAverage;
+            Property color = Property.newBuilder().setKey("rgb_color").setValue(colorCode).build();
+            Segment colored = Segment.newBuilder(s).addProperties(color).build();
+            segmentsWithColors.add(colored);
+        }
+        return Mesh.newBuilder().addAllVertices(verticesWithColors).addAllSegments(segmentsWithColors).build();
     }
 }
--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -3,12 +3,14 @@ package ca.mcmaster.cas.se2aa4.a2.visualizer;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 
 import java.awt.Graphics2D;
 import java.awt.Stroke;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 import java.util.List;
 
 public class GraphicRenderer {
@@ -27,6 +29,26 @@ public class GraphicRenderer {
             canvas.fill(point);
             canvas.setColor(old);
         }
+
+        /*
+        for (Segment s: aMesh.getSegmentsList()){
+            Line2D line = new Line2D.Double(59.2d, 99.8d, 419.1d, 99.8d);
+            canvas.fill(line);
+        }
+        */
+        for (Segment s : aMesh.getSegmentsList()) {
+            int vertex1Idx = s.getV1Idx();
+            int vertex2Idx = s.getV2Idx();
+
+            Vertex vertex1 = aMesh.getVertices(vertex1Idx);
+            Vertex vertex2 = aMesh.getVertices(vertex2Idx);
+            Color old = canvas.getColor();
+            
+            canvas.setColor(extractColor(s.getPropertiesList()));
+            canvas.drawLine((int) vertex1.getX(), (int) vertex1.getY(), (int) vertex2.getX(), (int) vertex2.getY());
+            canvas.setColor(old);
+        }
+
     }
 
     private Color extractColor(List<Property> properties) {
