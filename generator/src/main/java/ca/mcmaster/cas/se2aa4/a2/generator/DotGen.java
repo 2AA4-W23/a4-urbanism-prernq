e221af899a63af683d512e65dfba72deb4b82602
prernq <prernasprabhu@gmail.com>
2023-02-19 21:05:22 -0500

    Step 1 complete - successfully uses average colours of vertices to generate segment colours!

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Random;
+import java.util.List;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
@@ -55,12 +56,44 @@ public class DotGen {
         // Average colour segments
         ArrayList<Segment> segmentsWithColors = new ArrayList<>();
         for (Segment s : segments) {
-            int red = bag.nextInt(255);
-            int green = bag.nextInt(255);
-            int blue = bag.nextInt(255);
-            int RedAverage = red / 2;
-            int GreenAverage = green / 2;
-            int BlueAverage = blue / 2;
+
+            int vertex1Idx = s.getV1Idx();
+            int vertex2Idx = s.getV2Idx();
+            Vertex vertex1 = verticesWithColors.get(vertex1Idx);
+            Vertex vertex2 = verticesWithColors.get(vertex2Idx);
+
+            List<Property> properties_v1 = vertex1.getPropertiesList();
+            List<Property> properties_v2 = vertex2.getPropertiesList();
+            String color_code_v1 = null;
+            String color_code_v2 = null;
+            
+            for(Property p: properties_v1){
+                if(p.getKey().equals("rgb_color")){
+                    color_code_v1 = p.getValue();
+                }
+            }
+            for(Property p: properties_v2){
+                if(p.getKey().equals("rgb_color")){
+                    color_code_v2 = p.getValue();
+                }
+            }
+
+            
+            //String colour_code_v1 = vertex1.getProperties(vertex1Idx).getValue();
+            //String colour_code_v2 = vertex2.getProperties(vertex2Idx).getValue();
+            String[] colors_v1 = color_code_v1.split(",");
+            String[] colors_v2 = color_code_v2.split(",");
+            
+            int red_v1 = Integer.parseInt(colors_v1[0]);
+            int green_v1 = Integer.parseInt(colors_v1[1]);
+            int blue_v1 = Integer.parseInt(colors_v1[2]);
+            int red_v2 = Integer.parseInt(colors_v2[0]);
+            int green_v2 = Integer.parseInt(colors_v2[1]);
+            int blue_v2 = Integer.parseInt(colors_v2[2]);
+
+            int RedAverage = (red_v1 + red_v2) / 2;
+            int GreenAverage = (green_v1 + green_v2) / 2;
+            int BlueAverage = (blue_v1 + blue_v2) / 2;
             String colorCode = RedAverage + "," + GreenAverage + "," + BlueAverage;
             Property color = Property.newBuilder().setKey("rgb_color").setValue(colorCode).build();
             Segment colored = Segment.newBuilder(s).addProperties(color).build();
